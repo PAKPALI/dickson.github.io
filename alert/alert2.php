4096f253bfcc94d7d92ba2c73e52203f4664139b
PAKPALI <77837289+PAKPALI@users.noreply.github.com>
2021-01-22 11:43:18 +0000

    Add files via upload

---
+++ alert/alert2.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html>
+
+    <head>
+        <meta charset="utf-8" />
+        <title>ALERT</title>
+         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+
+        
+          <script src = " https://unpkg.com/sweetalert/dist/sweetalert.min.js " > </script > 
+          <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+          <script>
+            function checkforblank(){
+              if (document.getElementById('nom').value ==""){
+                alert('please name');
+                document.getElementById('nom').style.borderColor =" red";
+
+                return false;
+              }
+            }
+          </script>
+    </head>
+    <body>
+        
+    
+       
+
+      
+        <section style="margin-top: 30px;">
+        	<div class="section">
+
+              <aside>
+                <form  class="container border-top border-bottom bg-light " style="margin-top: 300px;" onsubmit="return checkforblank()">
+                  <div class="row">
+                    <div class="col-lg-4 offset-lg-4 mb-3">
+                      <label for = "TITRE" class="form-label"><h2>NOM</h2></label>
+                      <input type="text"  class="form-control" id ="nom" name="nom" placeholder="nom"  />
+                    </div>
+                  </div>
+
+                  <div class="row">
+                    <div class="col-lg-4 offset-lg-4 mb-3">
+                      <label for = "CONTENU" class="form-label"><h2>MOT DE PASSE:</h2></label>
+                      <input type="text"  class="form-control" id ="pass" name="pass" placeholder="mot de passe"  />
+                    </div>
+                  </div>
+                  <button  type="submit" style="margin-bottom: 10px; margin-left: 600px;" class="btn btn-primary btn-sm">AJOUTER</button>
+                  
+                </form>
+              </div>
+            </aside>
+
+        </section>
+
+       
+
+        
+        <script type="text/javascript" >
+        $('#ajouter').on('click' , function() {
+
+          Swal.fire({
+            icon: 'success',
+            title: 'GOOD',
+            text: 'bon travail!',
+          })
+        })
+        
+        </script>
+
+    </body>
+     
+</html>
